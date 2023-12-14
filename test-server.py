e647b14930ec23704c0e8355079dc203609861ba
Alan Van Art <alanv73@gmail.com>
2023-12-14 11:52:03 -0500

    refactor into methods: new clear_screen() method and server_choices(). Add R option to menu that reloads list of servers.

--- test-server.py
+++ test-server.py
@@ -1,33 +1,52 @@
 import speedtest
+import os
 
 s = speedtest.Speedtest(secure=True)
 
-servers = s.get_servers()
+def clear_screen():
+    # Clearing the Screen
+    
+    if(os.name == 'posix'): # posix is os name for Linux or mac
+        os.system('clear')
+    else: # else screen will be cleared for windows
+        os.system('cls')
 
-s1 = [servers[server] for server in servers]
-s2 = [svr for svrs in s1 for svr in svrs]
-names_only = [f'{s["sponsor"]:40} {s["name"]}' for s in s2]
+def server_choices():
+    servers = s.get_servers()
 
-print("\nSPEED TEST SERVERS")
-print(f"\n{'No.':^5} {'Sponsor':^40} {'Name':^25}")
-print(f"----- ---------------------------------------- -------------------------")
+    s1 = [servers[server] for server in servers]
+    s2 = [svr for svrs in s1 for svr in svrs]
+    names_only = [f'{s["sponsor"]:40} {s["name"]}' for s in s2]
 
-i = 0
-for svr in names_only:
-    i += 1
-    print(f"{i:<5} {svr}")
+    print("\nSPEED TEST SERVERS")
+    print(f"\n{'No.':^5} {'Sponsor':^40} {'Name':^25}")
+    print(f"----- ---------------------------------------- -------------------------")
+
+    i = 0
+    for svr in names_only:
+        i += 1
+        print(f"{i:<5} {svr}")
+    return s2
+
+clear_screen()
+servers = server_choices()
 
 s_num = 0
 
-while s_num < 1 or s_num > i:
-    user_input = input("\nSelect a Server (Q to quit): ")
+while s_num < 1 or s_num > len(servers):
+    user_input = input("\nSelect a Server (Q to quit, R to reload servers): ")
 
     if user_input.lower() == "q":
         exit()
+    if user_input.lower() == "r":
+        clear_screen()
+        server_choices()
+        continue
+    
 
     s_num = int(user_input)
 
-test_server = s2[s_num-1]
+test_server = servers[s_num-1]
 
 print("\nSELECTED SERVER")
 print("---------------")
